8cc0a7574df519c0262ed5e35af8520ab0f43ca7
Qianyi Sha <shhh9712@gmail.com>
2023-02-21 12:54:56 -0800

    clean datasets and model

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # NL2BASH-simple
 
-demo:
-https://huggingface.co/alexsha/t5-small-transferLearning-NLP2BASH
+Different Versions:
+https://huggingface.co/alexsha
--- customv2/data_process.py
+++ customv2/data_process.py
@@ -2,19 +2,26 @@ import os
 import re
 import pandas as pd
 
-data = {"output": [], "input":[]}
-dataset_path = "/Users/qsha/NL2BASH-simple/customv2"
-for file_name in os.listdir(dataset_path):
-    if '.txt' not in file_name:
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+input = []
+output = []
+for file_name in os.listdir(dir_path):
+    if ".txt" not in file_name:
         continue
-    with open(os.path.join(dataset_path, file_name)) as file:
+    with open(os.path.join(dir_path, file_name)) as file:
         for line in file.readlines():
             m = re.search("[0-9]*[.].", line)
             # print(line, m)
             # print(line[m.end(0):].split(' - '))
-            command, description = line[m.end(0):].split(' - ')
-            data["output"].append(command.strip())
-            data["input"].append(description.strip())
+            command, description = line[m.end(0) :].split(" - ")
+            input.append(command.strip())
+            output.append(description.strip())
 
 
-pd.DataFrame(data).to_csv(os.path.join(dataset_path, "customv2.csv"))
+pd.DataFrame(input).to_csv(
+    os.path.join(dir_path, "input.csv"), index=False, header=False
+)
+pd.DataFrame(output).to_csv(
+    os.path.join(dir_path, "output.csv"), index=False, header=False
+)
