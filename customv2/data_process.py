0ea0dcedc292357488336e156030a4723c9fb95f
Qianyi Sha <shhh9712@gmail.com>
2023-03-05 11:25:30 -0800

    Update custom v2

--- customv2/data_process.py
+++ customv2/data_process.py
@@ -9,14 +9,16 @@ output = []
 for file_name in os.listdir(dir_path):
     if ".txt" not in file_name:
         continue
+    print(file_name)
     with open(os.path.join(dir_path, file_name)) as file:
         for line in file.readlines():
-            m = re.search("[0-9]*[.].", line)
-            # print(line, m)
-            # print(line[m.end(0):].split(' - '))
-            command, description = line[m.end(0) :].split(" - ")
-            input.append(command.strip())
-            output.append(description.strip())
+            if len(line.strip()) > 0:
+                m = re.search("[0-9]*[.].", line)
+                print(line, m)
+                print(line[m.end(0) - 1 :].split(" -> "))
+                command, description = line[m.end(0) - 1 :].split(" -> ")
+                input.append(command.strip())
+                output.append(description.strip())
 
 
 pd.DataFrame(input).to_csv(
